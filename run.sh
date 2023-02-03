977de8c60f5b496b857d19435c5c21ab292b9c39
Thomas Champagne <th.champagne@gmail.com>
2023-02-03 13:45:52 +0100

    Typo run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ wget --no-cache https://raw.githubusercontent.com/notracking/hosts-blocklists/ma
 echo "$(date +'[%Y-%m-%d %H:%M:%S]') Domains blocklist updated"
 if [ ! -f ${CONF_DIR}/dns.conf ]; then echo "$(date +'[%Y-%m-%d %H:%M:%S]') ${CONF_DIR}/dns.conf not found. Use default dns.conf"; cp ${SAMPLE_CONF_DIR}/dns.conf ${CONF_DIR}; fi
 if [ ! -f ${CONF_DIR}/dhcp.conf ]; then echo "$(date +'[%Y-%m-%d %H:%M:%S]') ${CONF_DIR}/dhcp.conf not found. Use default dhcp.conf"; cp ${SAMPLE_CONF_DIR}/dhcp.conf ${CONF_DIR}; fi
-echo "${BLOCKLISTS_UPDATE_SCHEDULE}  /sbin/reboot" > /etc/crontabs/root
+echo "${BLOCKLISTS_UPDATE_SCHEDULE} /sbin/reboot" > /etc/crontabs/root
 /usr/sbin/crond
 /usr/sbin/dnsmasq --keep-in-foreground
